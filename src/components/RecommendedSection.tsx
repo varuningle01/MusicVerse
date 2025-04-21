23b2e4c04eee9e22563080661b5c7ad3b7cbf249
scorpion-varun <varuningle5@gmail.com>
2025-04-21 22:43:27 +0530

    Added slider for songs cards

--- src/components/RecommendedSection.tsx
+++ src/components/RecommendedSection.tsx
@@ -1,5 +1,6 @@
 import Card from "../child-components/Card";
 import { SongsList } from "../data/Songs";
+import { Swiper, SwiperSlide } from "swiper/react";
 
 const RecommendedSection = () => {
   return (
@@ -9,9 +10,30 @@ const RecommendedSection = () => {
         <p className="text-xs text-purple-400">View All</p>
       </div>
       <div className="flex flex-row gap-x-3">
-        {SongsList.map((item) => {
-          return <Card {...item} />;
-        })}
+        <Swiper
+          speed={1000}
+          breakpoints={{
+            320: { slidesPerView: 1.5 },
+            375: { slidesPerView: 1.75 },
+            420: { slidesPerView: 2 },
+            540: { slidesPerView: 2.5 },
+            640: { slidesPerView: 3.5 },
+            768: { slidesPerView: 3.5 },
+            1024: { slidesPerView: 4.5 },
+            1280: { slidesPerView: 5.5 },
+            1440: { slidesPerView: 6.5 },
+            2560: { slidesPerView: 12 },
+          }}
+          className="mySwiper"
+        >
+          {SongsList.map((item) => {
+            return (
+              <SwiperSlide key={item.id}>
+                <Card {...item} />
+              </SwiperSlide>
+            );
+          })}
+        </Swiper>
       </div>
     </div>
   );
